d7e05aa68e9f84049e84dcc9bf6b8b3f36f39e89
samiulakib <47288239+samiulakib@users.noreply.github.com>
2020-07-16 23:42:16 +0600

    Add files via upload

---
+++ about.php
@@ -0,0 +1,32 @@
+
+
+    <!-- Navigation -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header" align="center"><font color="#00FF33">ONLINE FACULTY FEEDBACK SYSTEM</font> </h1>
+                
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <!-- Intro Content -->
+        <div class="row" style="margin-bottom:50px;margin-left:50px">
+           
+               <P>Student Feedback system
+Here we have developed the a faculty feedback system, which is generally used in the university to rate the faculty based on the performance...Here we have 2 modules such as administrator, student.
+
+Administrator is the one who creates the student account by adding all student info and assigning the username and password. 
+Admin als0 checks the result once all students entered the feedback..
+You can perform all admin actions such as login to the account and check result..
+
+If you entere the student user and password and selected student option, then it will show all student information and let you enter the feedback based on the subject..
+
+
+</P> 
+               
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <!-- Footer -->
+     
---
+++ registration.php
@@ -0,0 +1,133 @@
+<?php 
+extract($_POST);
+if(isset($save))
+{
+//check user alereay exists or not
+$sql=mysqli_query($conn,"select * from user where email='$e'");
+
+$r=mysqli_num_rows($sql);
+
+if($r==true)
+{
+$err= "<font color='red'><h3 align='center'>This user already exists</h3></font>";
+}
+else
+{
+
+//encrypt your password
+$pass=md5($p);
+
+
+$query="insert into user values('','$n','$e','$pass','$mob','$pro','$sem','$gen',now())";
+mysqli_query($conn,$query);
+
+//upload image
+
+
+//mkdir("images/$e");
+//move_uploaded_file($_FILES['img']['tmp_name'],"images/$e/".$_FILES['img']['name']);
+
+
+$err="<font color='blue'><h3 align='center'>Registration successfull !!<h3></font>";
+
+}
+}
+
+
+
+
+?>
+
+
+		<div class="row">
+		<div class="col-sm-2"></div>
+		<div class="col-sm-8">
+		<form method="post" enctype="multipart/form-data">
+		<table class="table table-bordered" style="margin-bottom:50px">
+	<caption><h2 align="center">Registration Form</h2></caption>
+	<Tr>
+		<Td colspan="2"><?php echo @$err;?></Td>
+	</Tr>
+				
+				<tr>
+					<td>Enter Your name</td>
+					<Td><input  type="text" name="n" class="form-control" required/></td>
+				</tr>
+				<tr>
+					<td>Enter Your email </td>
+					<Td><input type="email" name="e" class="form-control" required/></td>
+				</tr>
+				
+				<tr>
+					<td>Enter Your Password </td>
+					<Td><input type="password" name="p" class="form-control" required/></td>
+				</tr>
+				
+				<tr>
+					<td>Enter Your Mobile </td>
+					<Td><input type="text" name="mob" class="form-control" required/></td>
+				</tr>
+				
+				<tr>
+					<td>Select Your Programme</td>
+					<Td><select name="pro" class="form-control" required>
+					
+					<option>CSE</option>
+					<option>EEE</option>
+					<option>BBA</option>
+					<option>ECO</option>
+					</select>
+					</td>
+				</tr>
+				
+				<tr>
+					<td>Select Your Semester</td>
+					<Td><select name="sem" class="form-control" required>
+					
+					<option>i</option>
+					<option>ii</option>
+					<option>iii</option>
+					<option>iv</option>
+					<option>v</option>
+					<option>vi</option>
+					<option>vii</option>
+					<option>viii</option>
+					<option>iX</option>
+					<option>x</option>
+					<option>xi</option>
+					<option>xii</option>
+					</select>
+					</td>
+				</tr>
+				
+				<tr>
+					<td>Select Your Gender</td>
+					<Td>
+				Male<input type="radio" name="gen" value="m"/>
+				Female<input type="radio" name="gen" value="f"/>	
+					</td>
+				</tr>
+				
+				
+				
+				
+				
+				
+				
+				
+				<tr>
+					
+					
+<Td colspan="2" align="center">
+<input type="submit" value="Save" class="btn btn-info" name="save"/>
+<input type="reset" value="Reset" class="btn btn-info"/>
+				
+					</td>
+				</tr>
+			</table>
+		</form>
+		</div>
+		<div class="col-sm-2"></div>
+		</div>
+	</body>
+</html>
